be672ec6e772e6d1c12980e8d2322cf86f8fbe62
boriskzd <boriskzd@users.noreply.github.com>
2024-03-21 13:22:27 +0100

    add GET merchants query

--- backend/merchantModel.js
+++ backend/merchantModel.js
@@ -9,4 +9,47 @@ const pool = new Pool({
 	password: process.env.DB_PASSWORD,
 	port: Number(process.env.DB_PORT), // files in .env are treated as strings, so port number has to be transformed from string to number
 });
-console.log(pool);
+
+// get all merchants from
+// WORK IN PROGRESS
+// export const getMerchants = async () => {
+// 	console.log("get merchants");
+
+// 	return await new Promise(function (myResolve, myReject) {
+// 		pool.query("SELECT * FROM merchants", (error, results) => {
+// 			if (error) {
+// 				myReject(error);
+// 			}
+// 			if (results) {
+// 				myResolve(results.row);
+// 			}
+// 		});
+// 	});
+// };
+
+// getMerchants();
+
+//get all merchants our database
+const getMerchants = async () => {
+	try {
+		return await new Promise(function (resolve, reject) {
+			pool.query("SELECT * FROM merchants", (error, results) => {
+				if (error) {
+					reject(error);
+				}
+				if (results && results.rows) {
+					resolve(results.rows);
+				} else {
+					reject(new Error("No results found"));
+				}
+			});
+		});
+	} catch (error_1) {
+		console.error(error_1);
+		throw new Error("Internal server error");
+	}
+};
+
+// test functions ( CANNOT TEST BY RUNNING node backend/merchantModel.js IN TERMINAL )
+// getMerchants();
+export { getMerchants };
