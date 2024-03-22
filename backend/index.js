38dbac1504b90bb10f2aea2a0a7f772a031caf7d
boriskzd <boriskzd@users.noreply.github.com>
2024-03-22 12:29:13 +0100

    fix CORS issue

--- backend/index.js
+++ backend/index.js
@@ -10,8 +10,9 @@ app.use(express.json());
 
 // Middleware that lets our Express app allow requests to this app from React
 app.use(function (req, res, next) {
-	// Set the Access-Control-Allow-Origin header to allow requests from http://localhost:3000 ( React in this case )
-	res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+	// Set the Access-Control-Allow-Origin header to allow requests from http://localhost:5173 ( React in this case )
+	// !!!!! Under no circumstance don't put ' / ' at the end of the link: ' http://localhost:5173 ', it will not work for CORS !!!!!
+	res.setHeader("Access-Control-Allow-Origin", `http://localhost:5173`);
 	// Set the Access-Control-Allow-Methods header to specify allowed HTTP methods
 	// TODO: add POST,PUT,DELETE,OPTIONS in allowed methods, besides GET
 	res.setHeader("Access-Control-Allow-Methods", "GET");
@@ -27,7 +28,6 @@ app.use(function (req, res, next) {
 app.get("/", (req, res) => {
 	getMerchants()
 		.then((response) => {
-			console.log(response);
 			res.status(200).send(response);
 		})
 		.catch((error) => {
